4207cd81d7195a2b50abab22e0fe6c11ea420509
WhosArjun <arjun.arunkumar.v@gmail.com>
2025-12-26 22:24:00 -0500

    Add files via upload

---
+++ TrashCan.java
@@ -0,0 +1,22 @@
+public class TrashCan{
+    public static void main(String[] args){
+        TrashCan t1 = new TrashCan(10);
+        System.out.println(t1.acceptTrash(11));
+    }
+
+    private double capacity;
+    private double currentWeight;
+
+    public TrashCan(double capacity){
+        this.capacity = capacity;
+        currentWeight = 0;
+    }
+
+    public double acceptTrash(double weight){
+        currentWeight += weight;
+        if(currentWeight>=capacity){
+            currentWeight = currentWeight - capacity;
+        }
+        return capacity-currentWeight;
+    }
+}
